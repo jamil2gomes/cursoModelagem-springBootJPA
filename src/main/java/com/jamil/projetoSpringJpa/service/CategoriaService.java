49b0531175483d8f7ce132ba381372f79d2244f4
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-27 16:26:41 -0300

    implementação dos metodos findById e findAll categoria

--- src/main/java/com/jamil/projetoSpringJpa/service/CategoriaService.java
+++ src/main/java/com/jamil/projetoSpringJpa/service/CategoriaService.java
@@ -1,8 +1,25 @@
 package com.jamil.projetoSpringJpa.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jamil.projetoSpringJpa.model.Categoria;
+import com.jamil.projetoSpringJpa.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
 
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria findBy(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+	
+	public List<Categoria>findAll(){
+		return repo.findAll();
+	}
 }
