ac98b52162871cb8c9917e11e833f895f7634c2c
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-27 15:20:04 -0300

    criação do projeto

---
+++ src/main/java/com/jamil/projetoSpringJpa/ProjetoSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jamil.projetoSpringJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamil/projetoSpringJpa/ProjetoSpringJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jamil.projetoSpringJpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoSpringJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
