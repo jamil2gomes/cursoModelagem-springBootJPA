df20ba44ebd0a75505bbfbda9f26d6380906e9ac
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-27 17:08:48 -0300

    inserção de produtos e categorias e seus relacionamentos

--- src/main/java/com/jamil/projetoSpringJpa/config/Seeder.java
+++ src/main/java/com/jamil/projetoSpringJpa/config/Seeder.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.jamil.projetoSpringJpa.model.Categoria;
+import com.jamil.projetoSpringJpa.model.Produto;
 import com.jamil.projetoSpringJpa.repository.CategoriaRepository;
+import com.jamil.projetoSpringJpa.repository.ProdutoRepository;
 
 @Configuration
 public class Seeder implements CommandLineRunner{
@@ -15,13 +17,32 @@ public class Seeder implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository catRepo;
 	
+	@Autowired
+	private ProdutoRepository prodRepo;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Categoria cat1 = new Categoria(null, "Escritorio");
 		Categoria cat2 = new Categoria(null, "Informatica");
 		
+		Produto p1 = new Produto(null, "Impressora", 800.00);
+		Produto p2 = new Produto(null, "Mouse", 20.00);
+		Produto p3 = new Produto(null, "Computador", 2000.00);
+		
+		cat1.adicionaProduto(p1);
+		cat2.adicionaProduto(p1);
+		cat2.adicionaProduto(p2);
+		cat2.adicionaProduto(p3);
+		
+		p1.adicionaCategoria(cat1);
+		p1.adicionaCategoria(cat2);
+		p2.adicionaCategoria(cat2);
+		p3.adicionaCategoria(cat2);
+		
+		
 		catRepo.saveAll(Arrays.asList(cat1, cat2));
+		prodRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
 	}
