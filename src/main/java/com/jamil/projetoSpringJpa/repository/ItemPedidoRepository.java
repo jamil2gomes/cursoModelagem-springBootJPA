6a9e17ce68e35deac88c004b9190bdd421aab1a1
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-28 11:52:28 -0300

    repositorio itempedido

---
+++ src/main/java/com/jamil/projetoSpringJpa/repository/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.jamil.projetoSpringJpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jamil.projetoSpringJpa.model.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+
+}
